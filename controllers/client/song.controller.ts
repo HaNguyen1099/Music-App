2994b7102b8373ff1076c248108027bca10a57ff
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-22 22:54:55 +0700

    Thống kê số lượt nghe bài hát bên client

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -140,3 +140,33 @@ export const favorite = async (req: Request, res: Response) => {
         res.redirect("/topics")
     }
 }
+
+// [PATCH] /songs/listen/:idSong
+export const listen = async (req: Request, res: Response) => {
+    try {
+        const idSong: string = req.params.idSong 
+
+        const song = await Song.findOne({
+            _id: idSong,
+            status: "active",
+            deleted: false
+        })
+
+        const listen: number =  song.listen + 1
+
+        await Song.updateOne({_id: idSong}, {listen: listen})
+        // like: ["id_user1", "id_user2"]
+
+        const newSong = await Song.findOne({
+            _id: idSong
+        })
+
+        res.json({
+            code: 200,
+            messgae: "Thành công!",
+            listen: newSong.listen
+        })
+    } catch (error) {
+        res.redirect("/topics")
+    }
+}
