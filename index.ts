e3890aa8c930a3b842e2dcb64e69d11a7e0be6b3
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-08-23 23:23:54 +0700

    Layout chung bên admin

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express, { Express } from "express"
 import dotenv from "dotenv"
+import path from "path"
 import * as database from "./config/database"
 import clientRoutes from "./routes/client/index.route"
+import adminRoutes from "./routes/admin/index.route"
+import { systemConfig } from "./config/system"
 
 dotenv.config()
 
@@ -15,7 +18,17 @@ app.use(express.static("public"))
 app.set("views", "./views")
 app.set("view engine", "pug")
 
+// TinyMCE
+app.use(
+    "/tinymce",
+    express.static(path.join(__dirname, "node_modules", "tinymce"))
+)
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
 clientRoutes(app)
+adminRoutes(app)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
